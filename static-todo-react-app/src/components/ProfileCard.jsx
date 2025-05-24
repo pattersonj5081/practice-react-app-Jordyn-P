ea7ed3365aef88e1780ca3edd5ff5b5f3cfe6f5b
pattersonj5081 <150544502+pattersonj5081@users.noreply.github.com>
2025-05-24 14:16:40 -0500

    Styling Added. Deliverable ready

--- static-todo-react-app/src/components/ProfileCard.jsx
+++ static-todo-react-app/src/components/ProfileCard.jsx
@@ -1,11 +1,11 @@
 function ProfileCard (props){
 const {name, age, bio} = props
 return(
-<div>
-<h2>
+<div >
+<h2 className="flex-container">
  Profile: 
 </h2>
-<p>
+<p className="flex-container">
  name:{name} <br/>
  age:{age}<br/>
  bio:{bio}
